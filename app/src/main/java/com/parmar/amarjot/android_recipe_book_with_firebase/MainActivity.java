a2122767e82d65068deeefe860250cf43533893d
Amarjot Parmar <amarjotparmar098@gmail.com>
2018-09-19 15:16:47 +1200

    create local/online recipe list fragments

--- app/src/main/java/com/parmar/amarjot/android_recipe_book_with_firebase/MainActivity.java
+++ app/src/main/java/com/parmar/amarjot/android_recipe_book_with_firebase/MainActivity.java
@@ -3,25 +3,37 @@ package com.parmar.amarjot.android_recipe_book_with_firebase;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            // Creating fragmentA and placing it on left side
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    System.out.println("Home Button Clicked");
+                    fragment_local_recipe_list localFragment = new fragment_local_recipe_list();
+                    fragmentTransaction.replace(R.id.fragmentLayout_main, localFragment);
+                    fragmentTransaction.commit();
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    System.out.println("Online Button Clicked");
+                    fragment_online_recipe_list onlineFragment = new fragment_online_recipe_list();
+                    fragmentTransaction.replace(R.id.fragmentLayout_main, onlineFragment);
+                    fragmentTransaction.commit();
                     return true;
             }
             return false;
@@ -33,9 +45,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-    }
 
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragment_local_recipe_list fragment = new fragment_local_recipe_list();
+        fragmentTransaction.add(R.id.fragmentLayout_main, fragment);
+        fragmentTransaction.commit();
+    }
 }
